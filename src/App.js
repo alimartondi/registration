b21c685cca9c7a80a2119c8afd3c2e36a0381a46
ali <alimartondi88@gmail.com>
2021-11-12 10:35:59 +0700

    Crete News App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import NewsDetail from "./pages/NewsDetail";
+import NewsList from "./pages/NewsList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/registration" exact element={<Registration />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/" exact element={<NewsList />} />
+          <Route path="/newsdetail/:id" exact element={<NewsDetail />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
